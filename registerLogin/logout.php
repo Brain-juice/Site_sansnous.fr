fce3dddea050223dc4354fc48d0a46474b00fa50
mathieulubert <mathieulubert@hotmail.com>
2018-07-05 02:53:15 +0200

    landingFinal

--- registerLogin/logout.php
+++
@@ -1,5 +0,0 @@
-<form action="#" method="post">
-	<input type="hidden" name="type" value="logout" />
-	<input type="hidden" name="id" value="<?= $user ?>" />
-	<input type="submit" value="Submit" />
-</form>
